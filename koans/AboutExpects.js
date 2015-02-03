fc14c7bdc16b632e1f173886b8fa94210234b70a
Michael Harris <mwharris333@gmail.com>
2015-02-03 12:40:50 -0500

    fix expects line 31

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -24,7 +24,7 @@ describe("About Expects", function() {
 
   //Sometimes you need to be really exact about what you "type".
   it("should assert equality with ===", function () { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = FILL_ME_IN.toString();
 	  var actualValue = (1 + 1).toString();
 	
   // toBe() will always use === to compare.
